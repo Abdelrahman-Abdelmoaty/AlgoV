10f35a6c89b2c67bd4f7b1a16970be7532ab0c72
Abdelrhman Abdelmoaty <Abdelr7manAbdelmoaty@gmail.com>
2023-12-10 20:40:17 +0200

    react-router-dom install&setup

--- algorithms-visualizer/src/App.js
+++ algorithms-visualizer/src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
+import { Route, Routes, Link } from "react-router-dom";
 
+const MockUp = () => {
+  return <div className="text-4xl text-slate-300">Mock Up</div>;
+};
 function App() {
   return (
     <div className="App">
-      <h1 className="font-bold text-5xl text-center">App</h1>
+      <a href="/">
+        <h1 className="font-bold text-5xl text-center my-12">App</h1>
+      </a>
+      <Link to="/graph/bfs">BFS</Link>
+      <Routes>
+        <Route path="/graph/bfs" element={<MockUp />} />
+      </Routes>
     </div>
   );
 }
