278c5e6f205bc6c69ec99a5e630e45f255688de0
omar <omarahmed2003eg@gmail.com>
2023-12-13 22:36:53 +0200

    removeNode, create&remove edge

--- algorithms-visualizer/src/components/Graph.js
+++ algorithms-visualizer/src/components/Graph.js
@@ -23,13 +23,11 @@ export default function () {
         setAdjMat(newAdjMat)
         setNodes([...nodes, { id }])
     }
-    // for undirected graph
-    const createEdge = (node1, node2) => {
-        const newAdjMat = [...adjMat]
-        newAdjMat[node1][node2] = 1;
-        newAdjMat[node2][node1] = 1;
-    }
     const printMat = () => {
+        if (nodes.length == 0){
+            console.log('no nodes');
+            return;
+        }
         console.log(adjMat.length, adjMat[0].length)
         adjMat.forEach(row => {
             let str = '';
@@ -37,9 +35,36 @@ export default function () {
             console.log(str);
         })
     }
+    const removeNode = (node) => {
+        const idx = nodes.findIndex(n => n.id === node)
+        const newMat = [...adjMat]
+        newMat.splice(idx, 1)
+        newMat.forEach(row => row.splice(idx, 1))
+        setAdjMat(newMat)
+        setNodes(nodes.filter(n => n.id !== node))
+    };
+    const createEdge = (node1, node2) => {
+        const idx1 = nodes.findIndex(n => n.id === node1)
+        const idx2 = nodes.findIndex(n => n.id === node2)
+        const newAdjMat = [...adjMat]
+        newAdjMat[idx1][idx2] = 1;
+        newAdjMat[idx2][idx1] = 1;
+    }
+    const removeEdge = (node1, node2)=>{
+        const idx1 = nodes.findIndex(n => n.id === node1)
+        const idx2 = nodes.findIndex(n => n.id === node2)
+        const newAdjMat = [...adjMat]
+        newAdjMat[idx1][idx2] = 0;
+        newAdjMat[idx2][idx1] = 0;
+    }
+    
     return <div>
-        <input value={newNode} onChange={(e) => setNewNode(e.target.value)} />
+        <input value={newNode} onChange={(e) => setNewNode(e.target.value)} /> 
+        <hr />
         <button onClick={() => addNode(+newNode)}>Add Node</button>
+        <hr />
         <button onClick={printMat}>Print</button>
+        <hr />
+        <button onClick={() => removeNode(+newNode)}>Remove node</button>
     </div>
 }
