4c19dd803330718db820b62a9a7f44ffa0a91f26
Abdullah Ayman <abdullahaymanmsre@gmail.com>
2023-12-19 23:17:39 +0200

    created general graph algorithm component

--- algorithms-visualizer/src/components/Graph/DFS.jsx
+++ algorithms-visualizer/src/components/Graph/DFS.jsx
@@ -1,18 +1,16 @@
 // import "rsuite/dist/styles/rsuite-default.css";
-import Graph from "./Graph";
-import { useEffect, useState } from "react";
-import Controls from "./Controls";
+import { useState } from "react";
+import GraphAlgorithm from "./GraphAlgorithm";
 
 export default function DFS() {
   const [nodes, setNodes] = useState([]);
-  const [edges, setEdges] = useState([]);
   const [adjMat, setAdjMat] = useState([]);
+  const [start, setStart] = useState(null);
+  const [stack, setStack] = useState([]);
 
   // const [neighborIdx, setNeighborIdx] = useState(null);
   // const [nodeIdx, setNodeIdx] = useState([]);
   const [visited, setVisited] = useState(new Set());
-  const [start, setStart] = useState(null);
-  const [stack, setStack] = useState([]);
 
   const visit = (idx) => {
     visited.add(idx);
@@ -46,49 +44,23 @@ export default function DFS() {
     }
   };
 
-  // Creating the Adjacency Matrix
-  useEffect(() => {
-    const newAdjMat = [];
-    nodes.forEach((node) => {
-      const row = [];
-      for (let j = 0; j < nodes.length; j++) row.push(0);
-      newAdjMat.push(row);
-    });
-
-    edges.forEach((edge) => {
-      const idx1 = nodes.findIndex((n) => n.id === edge.src);
-      const idx2 = nodes.findIndex((n) => n.id === edge.dest);
-
-      // TODO: check if not directed
-      newAdjMat[idx1][idx2] = 1;
-      newAdjMat[idx2][idx1] = 1;
-    });
-
-    setAdjMat(newAdjMat);
-  }, [edges, nodes]);
-
   return (
     <div class="dfs-component">
       <h2>Depth First Search Visualization</h2>
-      <main>
-        <Controls
-          start={() => {
-            initDfs();
-          }}
-          startV={nodes[start]?.value}
-          setStart={(val) => setStart(nodes.findIndex((n) => n.value === val))}
-          next={() => {
-            dfs();
-          }}
-          back={() => {}}
-        />
-        <Graph
-          nodes={nodes}
-          setNodes={setNodes}
-          edges={edges}
-          setEdges={setEdges}
-        />
-      </main>
+      <GraphAlgorithm
+        back={() => {}}
+        start={() => {
+          initDfs();
+        }}
+        next={() => {
+          dfs();
+        }}
+        setAdjMat={setAdjMat}
+        setStartIdx={setStart}
+        startIdx={start}
+        nodes={nodes}
+        setNodes={setNodes}
+      />
     </div>
   );
 }
---
+++ algorithms-visualizer/src/components/Graph/GraphAlgorithm.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import Controls from "./Controls";
+import Graph from "./Graph";
+
+export default function GraphAlgorithm({
+  start,
+  back,
+  next,
+  setAdjMat,
+  startIdx,
+  setStartIdx,
+  nodes,
+  setNodes,
+}) {
+  const [edges, setEdges] = useState([]);
+
+  // Creating the Adjacency Matrix
+  useEffect(() => {
+    const newAdjMat = [];
+    nodes.forEach((node) => {
+      const row = [];
+      for (let j = 0; j < nodes.length; j++) row.push(0);
+      newAdjMat.push(row);
+    });
+
+    edges.forEach((edge) => {
+      const idx1 = nodes.findIndex((n) => n.id === edge.src);
+      const idx2 = nodes.findIndex((n) => n.id === edge.dest);
+
+      // TODO: check if not directed
+      newAdjMat[idx1][idx2] = 1;
+      newAdjMat[idx2][idx1] = 1;
+    });
+
+    setAdjMat(newAdjMat);
+  }, [edges, nodes]);
+
+  return (
+    <main>
+      <Controls
+        start={start}
+        startV={nodes[startIdx]?.value}
+        setStart={(val) => setStartIdx(nodes.findIndex((n) => n.value === val))}
+        next={next}
+        back={back}
+      />
+      <Graph
+        nodes={nodes}
+        setNodes={setNodes}
+        edges={edges}
+        setEdges={setEdges}
+      />
+    </main>
+  );
+}
