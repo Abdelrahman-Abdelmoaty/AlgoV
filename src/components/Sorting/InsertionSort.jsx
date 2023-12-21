d19bd9f62d57a8efc744273e85b05a8de903f0c3
Abdullah Ayman <abdullahaymanmsre@gmail.com>
2023-12-22 00:47:26 +0200

    created list component for sorting algorithms

--- src/components/Sorting/InsertionSort.jsx
+++ src/components/Sorting/InsertionSort.jsx
@@ -1,9 +1,59 @@
+import { useState } from "react";
 import List from "./List";
 
 export default function InsertionSort() {
+  const [list, setList] = useState([
+    { id: "1", value: 1 },
+    { id: "2", value: 2 },
+    { id: "3", value: 3 },
+    { id: "4", value: 1 },
+    { id: "5", value: 2 },
+    { id: "6", value: 3 },
+    { id: "7", value: 1 },
+    { id: "8", value: 2 },
+    { id: "9", value: 3 },
+  ]);
+  const [swap, setSwap] = useState([]);
+
   return (
     <div>
-      <List />
+      <List
+        list={list}
+        setList={setList}
+        setSwapList={setSwap}
+        swapList={swap}
+        areas={[
+          {
+            i: 1,
+            j: 2,
+            color: "red",
+          },
+          {
+            i: 3,
+            j: 9,
+            color: "blue",
+          },
+        ]}
+      />
+      <button
+        onClick={() =>
+          setList([
+            { id: "1", value: 1 },
+            { id: "2", value: 2 },
+            { id: "3", value: 3 },
+            { id: "4", value: 1 },
+            { id: "5", value: 2 },
+            { id: "6", value: 3 },
+            { id: "7", value: 1 },
+            { id: "8", value: 2 },
+            { id: "9", value: 3 },
+          ])
+        }
+      >
+        create
+      </button>
+      <button onClick={() => setSwap([{ i: 1, j: 5 }])}>swap</button>
+      <button onClick={() => setSwap([...swap, { i: 0, j: 2 }])}>swap2</button>
     </div>
   );
 }
--- src/components/Sorting/List.jsx
+++ src/components/Sorting/List.jsx
@@ -1,3 +1,145 @@
-export default function List() {
-  return <div>Hello from list</div>;
+import { useEffect, useState } from "react";
+import "../../styles/list.css";
+import uuid from "react-uuid";
+/*
+    Sample List Item: {
+        id: string,
+        value: number,
+        color?: string,
+        backgroundColor?: string,
+        borderColor?: string,
+    }
+
+    ? areas must be sorted with no interference
+    Sample Area Item: {
+        color: string
+        i: number
+        j: number
+    }
+
+    Sample swapList Item: {
+        i: number,
+        j: number
+    }
+*/
+export default function List({ list, setList, swapList, setSwapList, areas }) {
+  const [state, setState] = useState(0);
+  const [swapping, setSwapping] = useState(null);
+  const [swapAgain, setSwapAgain] = useState(0);
+  const [test, setTest] = useState(false);
+
+  useEffect(() => {
+    if (swapping && swapAgain) {
+      const newList = [...list];
+      newList[swapping.i] = { ...newList[swapping.i], bottom: "60px", left: 0 };
+      newList[swapping.j] = {
+        ...newList[swapping.j],
+        bottom: "-60px",
+        left: 0,
+      };
+      setList(newList);
+      setState(1);
+    }
+  }, [swapping]);
+
+  useEffect(() => {
+    switch (state) {
+      case 1:
+        setTimeout(() => {
+          let num = swapping.j - swapping.i;
+          const newList = [...list];
+          newList[swapping.i] = {
+            ...newList[swapping.i],
+            left: `${55 * num}px`,
+          };
+          newList[swapping.j] = {
+            ...newList[swapping.j],
+            left: `-${55 * num}px`,
+          };
+          setList(newList);
+          setState(2);
+        }, 1000);
+        break;
+      case 2:
+        setTimeout(() => {
+          const newList = [...list];
+          newList[swapping.i] = { ...newList[swapping.i], bottom: 0 };
+          newList[swapping.j] = { ...newList[swapping.j], bottom: 0 };
+          setList(newList);
+          setState(3);
+        }, 1000);
+        break;
+      case 3:
+        setTimeout(() => {
+          const newList = [...list];
+          const iv = { ...newList[swapping.i], id: uuid(), left: 0, bottom: 0 };
+          newList[swapping.i] = {
+            ...newList[swapping.j],
+            id: uuid(),
+            left: 0,
+            bottom: 0,
+          };
+          newList[swapping.j] = iv;
+          setList(newList);
+        }, 1000);
+        break;
+    }
+  }, [state]);
+
+  useEffect(() => {
+    if (swapList.length > 0) {
+      setSwapping(swapList[0]);
+      setTimeout(() => {
+        setSwapAgain(swapAgain + 1);
+        setTest(!test);
+      }, 2000);
+    }
+  }, [swapAgain]);
+
+  useEffect(() => {
+    console.log("from here", swapAgain, " list: ", swapList);
+    if (swapList.length === 0) setSwapAgain(0);
+    else if (swapList.length > 0 && swapAgain === 0) setSwapAgain(1);
+  }, [swapList, swapAgain]);
+
+  useEffect(() => {
+    setSwapList(swapList.slice(1, swapList.length));
+  }, [test]);
+
+  let itemI = 0;
+  let areaI = 0;
+  let res = [];
+  while (itemI < list.length) {
+    if (areas && areas[areaI].i === itemI) {
+      res.push(
+        <div key={areaI} style={{ border: `2px solid ${areas[areaI].color}` }}>
+          {Array(areas[areaI].j - areas[areaI].i)
+            .fill(1)
+            .map((x, i) => {
+              const item = list[areas[areaI].i + i];
+              return (
+                <li key={item.id} style={item}>
+                  {item.value}
+                </li>
+              );
+            })}{" "}
+        </div>
+      );
+      itemI = areas[areaI].j;
+      areaI++;
+    } else {
+      res.push(
+        <li key={list[itemI].id} style={list[itemI]}>
+          {list[itemI].value}
+        </li>
+      );
+      itemI++;
+    }
+  }
+
+  return (
+    <div className="list-component">
+      <ul className="list">{res.map((i) => i)}</ul>
+    </div>
+  );
 }
