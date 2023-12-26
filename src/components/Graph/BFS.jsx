d29a916ff02a869d177883d77bacf68ecd587694
omar <omarahmed2003eg@gmail.com>
2023-12-26 23:17:06 +0200

    enhanced bfs

--- src/components/Graph/BFS.jsx
+++ src/components/Graph/BFS.jsx
@@ -13,68 +13,72 @@ export default function BFS() {
   const defaultBorderColor = "black";
   const neighborBorderColor = "#08ff00";
   const currentBorderColor = "red";
-  const visitedColor = "rgb(44, 202, 227)";
+  const initialColor = [0, 150, 170];
+  const gradientStep = 35;
 
   useEffect(() => {
     if (!queue.isEmpty()) bfs();
   }, [queue]);
 
-  const visit = (idx, nodes) => {
+  const calculateGradientColor = (levelWithinSameLevel) => {
+    const newColor = initialColor.map((value) => Math.min(255, value + (levelWithinSameLevel % 7) * gradientStep));
+    return `rgb(${newColor.join(",")})`;
+  };
+
+  const visit = (idx, nodes, level) => {
     visited.add(idx);
-    nodes[idx].color = visitedColor;
+    nodes[idx].color = calculateGradientColor(level);
   };
 
   const initBfs = () => {
     let newQueue = new Queue();
-    newQueue.enqueue(start);
+    newQueue.enqueue({ vertex: start, level: 0 });
     setQueue(newQueue);
     setVisited(new Set());
   };
 
   const bfs = () => {
     const newNodes = [...nodes];
-    const currentVertex = queue.peek();
-
-    if (lastVertex !== null) {
-      newNodes[lastVertex].borderColor = defaultBorderColor;
-      for (let i = 0; i < nodes.length; i++) {
-        if (adjMat[lastVertex][i] === 1 && !visited.has(i)) {
-          newNodes[i].borderColor = defaultBorderColor;
-        }
-      }
-    }
     if (!queue.isEmpty()) {
-      visit(currentVertex, newNodes);
-
-      for (let i = 0; i < nodes.length; i++) {
-        if (adjMat[currentVertex][i] === 1 && !visited.has(i)) {
-          newNodes[i].borderColor = neighborBorderColor;
-          queue.enqueue(i);
-        }
+      const { vertex: currentVertex, level } = queue.peek();
+      if (!visited.has(currentVertex)) {
+        visit(currentVertex, newNodes, level);
       }
-
       if (lastVertex !== null) {
         newNodes[lastVertex].borderColor = defaultBorderColor;
+        for (let i = 0; i < nodes.length; i++) {
+          if (adjMat[lastVertex][i] === 1 && !visited.has(i)) {
+            newNodes[i].borderColor = defaultBorderColor;
+          }
+        }
       }
-
+      for (let i = 0; i < nodes.length; i++) {
+        if (adjMat[currentVertex][i] === 1 && !visited.has(i)) {
+          if (!visited.has(i)) {
+            newNodes[i].borderColor = neighborBorderColor;
+          }
+          queue.enqueue({ vertex: i, level: level + 1 });
+        }
+      }
+      if (lastVertex !== null)
+        newNodes[lastVertex].borderColor = defaultBorderColor;
       newNodes[currentVertex].borderColor = currentBorderColor;
       setLastVertex(currentVertex);
-
       queue.dequeue();
       setNodes(newNodes);
     }
   };
-
   return (
     <div className="dfs-component">
-      <h2 className="algorithm-title">Breadth First Search Visualization</h2>
+      <h2>Breadth First Search Visualization</h2>
       <GraphAlgorithm
-        back={() => {}}
+        back={() => { }}
         start={() => {
           initBfs();
         }}
         next={() => {
-          bfs();
+          if (!queue.isEmpty())
+            bfs();
         }}
         setAdjMat={setAdjMat}
         setStartIdx={setStart}
@@ -83,7 +87,7 @@ export default function BFS() {
         setNodes={setNodes}
         keys={[
           {
-            backgroundColor: visitedColor,
+            backgroundColor: calculateGradientColor(0),
             label: "Visited",
           },
           {
@@ -99,205 +103,3 @@ export default function BFS() {
     </div>
   );
 }
-
-/*
-import { Queue } from "../../lib/utils";
-import GraphAlgorithm from "./GraphAlgorithm";
-import React, { useState } from "react";
-
-export default function BFS() {
-  const [directed, setDirected] = useState(false);
-  const [nodes, setNodes] = useState([]);
-  const [adjMat, setAdjMat] = useState([]);
-  const [start, setStart] = useState(null);
-  const [queue, setQueue] = useState(new Queue());
-  const [visited, setVisited] = useState(new Set());
-  const [levelColors, setLevelColors] = useState({ 0: "grey" });
-
-  const visit = (idx, level) => {
-    const newNodes = [...nodes];
-    const color = levelColors[level];
-    newNodes[idx] = { ...newNodes[idx], color };
-    setNodes(newNodes);
-  };
-
-  const initBfs = () => {
-    let newQueue = new Queue();
-    newQueue.enqueue({ node: start, level: 0 });
-    setQueue(newQueue);
-    setVisited(new Set());
-    bfs();
-  };
-
-  const bfs = () => {
-    if (!queue.isEmpty()) {
-      const { node, level } = queue.peek();
-      const newQueue = queue.copy();
-      if (!visited.has(node)) {
-        visit(node, level);
-        setVisited(new Set(visited).add(node));
-      }
-
-      for (let i = 0; i < nodes.length; i++) {
-        if (adjMat[node][i] === 1 && !visited.has(i)) {
-          newQueue.enqueue({ node: i, level: level + 1 });
-        }
-      }
-
-      newQueue.dequeue();
-      setQueue(newQueue);
-
-      if (!levelColors.hasOwnProperty(level + 1)) {
-        setLevelColors((prev) => ({
-          ...prev,
-          [level + 1]: getRandomColor(),
-        }));
-      }
-    }
-  };
-
-  const getRandomColor = () => {
-    const letters = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) color += letters[Math.floor(Math.random() * 16)];
-    return color;
-  };
-
-  return (
-    <div className="dfs-component">
-      <h2>Breadth First Search Visualization</h2>
-      <GraphAlgorithm
-        back={() => {}}
-        start={() => {
-          initBfs();
-        }}
-        next={() => {
-          bfs();
-        }}
-        setAdjMat={setAdjMat}
-        setStartIdx={setStart}
-        startIdx={start}
-        nodes={nodes}
-        setNodes={setNodes}
-      />
-    </div>
-  );
-}
-*/
-
-/*
-import { Queue } from "../../lib/utils";
-import Graph from "./Graph";
-import { useState } from "react";
-import GraphAlgorithm from "./GraphAlgorithm";
-
-export default function BFS() {
-  const [nodes, setNodes] = useState([]);
-  const [adjMat, setAdjMat] = useState([]);
-  const [start, setStart] = useState(null);
-  const [queue, setQueue] = useState(new Queue());
-  const [visited, setVisited] = useState(new Set());
-
-  const visit = (idx) => {
-    visited.add(idx);
-    const newNodes = [...nodes];
-    newNodes[idx] = { ...newNodes[idx], color: "grey" };
-    setNodes(newNodes);
-  };
-
-  const initBfs = () => {
-    let newQueue = new Queue();
-    newQueue.enqueue(start);
-    setQueue(newQueue);
-    setVisited(new Set());
-    bfs();
-  };
-
-  const bfs = () => {
-    if (!queue.isEmpty()) {
-      const currentVertex = queue.peek();
-      const newQueue = queue.copy();
-      visit(currentVertex);
-      for (let i = 0; i < nodes.length; i++) {
-        if (adjMat[currentVertex][i] === 1 && !visited.has(i)) {
-          newQueue.enqueue(i);
-        }
-      }
-      newQueue.dequeue();
-      setQueue(newQueue);
-    }
-  };
-
-
-  return (
-    <div class="dfs-component">
-      <h2>Breadth First Search Visualization</h2>
-      <GraphAlgorithm
-        back={() => {}}
-        start={() => {
-          initBfs();
-        }}
-        next={() => {
-          bfs();
-        }}
-        setAdjMat={setAdjMat}
-        setStartIdx={setStart}
-        startIdx={start}
-        nodes={nodes}
-        setNodes={setNodes}
-      />
-    </div>
-  );
-}
-*/
-
-/*
-import { Queue } from "../../lib/utils";
-import Graph from "./Graph";
-import { useState } from "react";
-export default function BFS() {
-  const [nodes, setNodes] = useState([]);
-  const [edges, setEdges] = useState([]);
-  const handleClick = ()=>{
-      const adjMat = Array.from({ length: nodes.length + 1 }, () => Array(nodes.length + 1).fill(0));
-      const visited = Array(nodes.length + 1).fill(0);
-      for (let i = 0; i < edges.length; i++) {
-        adjMat[edges[i].src][edges[i].dest] = 1;
-        adjMat[edges[i].dest][edges[i].src] = 1;
-      }
-      let src = 1;
-      visited[src] = 1;
-      let q = new Queue();
-      q.enqueue(src);
-      while (!q.isEmpty) {
-        const u = q.peek();
-        q.dequeue();
-        setNodes((prev) => {
-          return prev.map((node) => {
-            if (node.id == u) {
-              return { ...node, color: "red" };
-            } else {
-              return node;
-            }
-          });
-        });
-        for (let i = 1; i <= nodes.length; i++) {
-          if (adjMat[u][i] == 1 && !visited[i]) {
-            visited[i] = 1;
-            q.enqueue(i);
-          }
-        }
-      }
-    }
-  return (
-    <div>
-      <button
-        className="text-3xl"
-        onClick={handleClick}>
-        bfs
-      </button>
-      <Graph nodes={nodes} setNodes={setNodes} edges={edges} setEdges={setEdges} />
-    </div>
-  );
-}
-*/
